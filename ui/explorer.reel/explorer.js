3a2b69a3ffa5c154ee33827372756727fab0351d
PiTOG <noreply>
2014-05-02 00:13:24 +0000

    Add component explorer

---
+++ ui/explorer.reel/explorer.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/explorer.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Explorer
+ * @extends Component
+ */
+exports.Explorer = Component.specialize(/** @lends Explorer# */ {
+    constructor: {
+        value: function Explorer() {
+            this.super();
+        }
+    }
+});
